d00ec793756a7f3e39520f3f127e0373d328c76d
James Halsall <james.halsall@inviqa.com>
2018-01-23 09:48:55 +0100

    PS-2017.3.1 <jameshalsall@192.168.2.5 Create baseRefactoring.xml, markdown.xml, PHP Interface Doc Comment.php, PHP Class.php, IntelliLang.xml, ide.general.xml, project.default.xml, diff.xml, git.xml, other.xml, PHP Class Doc Comment.php, Default.xml, PHP Field Doc Comment.php, find.xml, ui.lnf.xml, filetypes.xml, php.xml, javaeeExternalResources.xml, code.style.schemes.xml, colors.scheme.xml, notifications.xml, PHP Trait.php, databaseDrivers.xml, Default.xml, PHPUnit Test.php, PHP File Header.php, laf.xml, Default _1_.xml, web-browsers.xml, vcs.xml, editor.codeinsight.xml, PHP Interface.php, databaseSettings.xml, keymap.xml, editor.xml, usageView.xml, debugger.xml

---
+++ fileTemplates/includes/PHP Class Doc Comment.php
@@ -0,0 +1,7 @@
+/**
+ * 
+#if (${NAMESPACE}) * @package ${NAMESPACE}
+#else * @package 
+#end
+ * @author  James Halsall <james.halsall@inviqa.com>
+ */
---
+++ fileTemplates/includes/PHP Interface Doc Comment.php
@@ -0,0 +1,6 @@
+/**
+ * Interface ${NAME}
+#if (${NAMESPACE}) * @package ${NAMESPACE}
+#end
+ * @author James Halsall <james.halsall@inviqa.com>
+ */
